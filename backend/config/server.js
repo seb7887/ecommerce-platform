e664d3ea63c29942a3ecddbe7314c46fd3d28c24
Sebastian Matias Segura <segurasebastian7887@gmail.com>
2020-11-27 14:03:15 -0300

    Change Strapi backend url env variable

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,10 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+module.exports = ({ env, config }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '1660ba13551ff7aacec1a4d171b763f8'),
+      secret: env("ADMIN_JWT_SECRET", "1660ba13551ff7aacec1a4d171b763f8"),
     },
   },
+  url: env("BACKEND_URL", "http://localhost:1337"),
 });
